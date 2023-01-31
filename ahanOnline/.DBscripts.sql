26c54ffe0291b938a54249b48cdd8575d2bd0503
Saeed Matan <saeed.matan@gmail.com>
2023-01-31 08:50:33 +0330

    there's a folder contains answer an DB files.

---
+++ ahanOnline/.DBscripts.sql
@@ -0,0 +1,40 @@
+﻿/* first, initial data is imported from excel to SSMS as a tables into "NEW1 database" and then some tables are converted.
+ََAlso, the relations are created between the tables by creating primary and foreign key.*/
+
+
+-- converting data of Customers
+insert into Customers(name)
+select distinct Customer from [dbo].[Sales$] where Customer not in (select name from Customers)
+
+
+--converting data of Products
+insert into Products(name)
+select distinct Product from [dbo].[Sales$] where Product not in (select [name] from Products)
+
+--converting data of managers
+insert into managers(name)
+select distinct manager
+from [dbo].[organizationchart$]
+where manager not in (select [name] from managers) and manager <> 'NULL'
+
+--converting data of Sales
+insert into Sales(
+       [OrderId]
+      ,[CustomerId]
+      ,[ProductId]
+      ,[Date]
+      ,[Quantity]
+      ,[UnitPrice]
+		)
+select s.OrderID, c.id, p.id, s.date, s.Quantity, s.UnitPrice from [dbo].[Sales$] s 
+join Customers c on s.Customer = c.name
+join Products p on s.Product = p.name
+
+--converting data of SaleProfit
+insert into SaleProfit(ProductId,ProfitRatio)
+select p.id, sp.ProfitRatio from [dbo].[SalesProfit$] sp
+join [dbo].[Products] p on sp.Product = p.name
+
+insert into SaleProfit(ProductId,ProfitRatio)
+select DISTINCT id, 0.1 from Products where id not in (select id from SaleProfit)
+
---
+++ ahanOnline/answers.sql
@@ -0,0 +1,73 @@
+--create a DB called new and three table called Sales, SaleProfit, OrgChart
+CREATE DATABASE new
+USE new
+CREATE TABLE Sales(
+		SalesID int NOT NULL IDENTITY(1,1),
+		OrderID int NOT NULL,
+		Customer varchar(10),
+		Product varchar(10),
+		[Date] Date,
+		Quantity tinyint,
+		UnitPrice int,
+		);
+
+CREATE TABLE SaleProfit(
+		Product varchar(10),
+		ProfitRatio int
+		);
+/*  tip: The query below inserts 10% profit as record of ProfitRatio column 
+	for values of Product column(in Sales table) that don't exist in SaleProfit*/
+	select*from saleprofit
+INSERT INTO SaleProfit(Product, ProfitRatio)
+SELECT distinct Product, 10 FROM Sales WHERE Product NOT IN(SELECT distinct Product FROM SaleProfit);
+
+CREATE TABLE OrgChart(
+		id int NOT NULL IDENTITY(1,1),
+		[name] nvarchar(50),
+		manager nvarchar(50),
+		ManagerId tinyint
+		);
+
+
+-- section1
+SELECT SUM(Quantity*UnitPrice) AS TotalSales
+FROM Sales;
+
+-- sectoin2
+SELECT COUNT(DISTINCT CustomerId) AS CountOfCustomers
+FROM Sales;
+
+-- section3
+SELECT p.name AS ProductName, SUM(s.Quantity*s.UnitPrice) AS ProductSales
+FROM Sales s
+join Products p on s.ProductId = p.id
+Group BY p.name;
+
+--section4
+SELECT c.name, SUM(s.Quantity*s.UnitPrice) AS PurchaseAmount,
+COUNT(DISTINCT s.OrderID) AS NumOfInvoices, SUM(s.Quantity) AS SumOfQTY
+FROM Sales s
+join Customers c on s.CustomerId = c.id
+WHERE s.CustomerId in(
+				 select CustomerId
+				 from Sales 
+				 where Quantity*UnitPrice>1500
+				 )
+Group BY c.name;
+
+-- section5
+select SUM(s.Quantity*s.UnitPrice*sp.ProfitRatio) AS ProfAmount,
+round(SUM(s.Quantity*s.UnitPrice*sp.ProfitRatio)/SUM(s.Quantity*s.UnitPrice)*100,3) AS ProfPercent
+from Sales s
+join SaleProfit sp on s.ProductId = sp.ProductId;
+
+--section6
+select SUM(s.CountOfCust)
+from(
+	select count(distinct CustomerId) AS CountOfCust, date
+	from Sales
+	group by date
+) s;
+
+--section7
+https://stackoverflow.com/questions/3309405/tough-t-sql-to-display-org-chart-hierarchy-recursion
